000556ca4dab5ce3a33dc0f569b5f833e21f5574
Nikita <znikidi@gmail.com>
2024-12-17 13:28:35 +0500

    start working with add function

--- main.cpp
+++ main.cpp
@@ -52,7 +52,13 @@ long hashFunction(const std::vector<long> vec)
     return result;
 }
 
+std::vector<long> sub(std::vector<long> &first, std::vector<long> &second, const long base)
+{
+    
+}
+
 std::vector<long> karatsubaMult(std::vector<long> &first, std::vector<long> &second, const long base)
+// Метод карацубы
 {
     
     const long len1 = first.size();
@@ -65,7 +71,39 @@ std::vector<long> karatsubaMult(std::vector<long> &first, std::vector<long> &sec
     /*Для маленьких чисел данный алгоритм не имеет смысла, т.к. даже наивный алгоритм целесообразнее (быстрее)*/
     if (len <= 10) return naiveMult(first, second, base); // Литерал нужно подобрать, протестировав данные
 
-    //13-19
+    long halflen = len / 2;
+    std::vector<long> firstLow(first); // малый разряд, наполняем разрядами из числа first
+    firstLow.resize(halflen, 0); // Оставляем только пол массива, заполняем нулями
+    std::vector<long> SecondLow(first); // малый разряд
+    SecondLow.resize(halflen, 0);
+    std::vector<long> FirstHigh(len1 > halflen ? first.begin() : first.end(), first.end()); // Старший разряд, нужно начать с половинки, для этого надо проверить
+    FirstHigh.resize(halflen, 0);
+    std::vector<long> SecondHigh(len2 > halflen ? second.begin() : second.end(), second.end()); // Старший разряд, нужно начать с половинки, для этого надо проверить
+    SecondHigh.resize(halflen, 0);
+
+    std::vector<long> multLow = karatsubaMult(firstLow, SecondLow, base);
+    std::vector<long> multHigh = karatsubaMult(firstLow, SecondHigh, base);
+
+    std::vector<long> firstSum = add(firstLow, FirstHigh, base);
+    std::vector<long> SecondSum = add(SecondLow, SecondHigh, base);
+    std::vector<long> multMid = karatsubaMult(firstSum, SecondSum, base);
+    multMid = sub(multMid, multLow, base);
+    multMid = sub(multMid, multHigh, base);
+
+    std::vector<long> result(2 * len, 0);
+
+    for (long i = 0; i < 2 * len - 1; i++) {
+        result[i] += (i < halflen) ? multLow[i] : 0;
+        result[i] += (i < halflen && i < halflen + multMid.size()) ? multMid[i - halflen] : 0;
+        result[i] += (i < len && i < len + multHigh.size()) ? multHigh[i - len] : 0;
+        result[i + 1] += result[i] / base;
+        result[i] %= base;
+    }
+
+    result[2 * len - 1] += multHigh[len - 1];
+    while (result[result.size() - 1] == 0) result.pop_back();
+
+    return result;
 }
 
 std::vector<long> naiveMult(std::vector<long> &first, std::vector<long> &second, const long base)
