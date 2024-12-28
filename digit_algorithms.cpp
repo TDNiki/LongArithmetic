2ad02a35fc13517fc7a29b5c51c08df2aea3b226
TDNiki <Nikid-z@yandex.ru>
2024-12-28 15:32:29 +0500

    fixed problem with <0 digits. (hashsum)

--- digit_algorithms.cpp
+++ digit_algorithms.cpp
@@ -1,14 +1,14 @@
 #include <vector>
 
-std::vector<long> add(std::vector<long> &first, std::vector<long> &second, const long base)
+std::vector<long long> add(std::vector<long long> &first, std::vector<long long> &second, const long long base)
 // Суммирование
 {
-    const long len1 = first.size();
-    const long len2 = first.size();
-    long len = std::max(len1, len2);
-    std::vector<long> result(first);
+    const long long len1 = first.size();
+    const long long len2 = first.size();
+    long long len = std::max(len1, len2);
+    std::vector<long long> result(first);
     result.resize(len + 1, 0);
-    for (long i = 0; i < len2; i++) {
+    for (long long i = 0; i < len2; i++) {
         result[i] += second[i];
         result[i + 1] += result[i] / base;
         result[i] %= base;
@@ -19,13 +19,13 @@ std::vector<long> add(std::vector<long> &first, std::vector<long> &second, const
     return result;
 }
 
-std::vector<long> sub(std::vector<long> &first, std::vector<long> &second, const long base)
+std::vector<long long> sub(std::vector<long long> &first, std::vector<long long> &second, const long long base)
 // Вычитание
 {
-    const long len1 = first.size();
-    const long len2 = first.size();
-    std::vector<long> result(first);
-    for (long i = 0; i < len1 - 1; i++) {
+    const long long len1 = first.size();
+    const long long len2 = first.size();
+    std::vector<long long> result(first);
+    for (long long i = 0; i < len1 - 1; i++) {
         result[i] += base - (i < len2 ? second[i] : 0);
         result[i + 1] += result[i] / base - 1;
         result[i] %= base;
@@ -38,15 +38,15 @@ std::vector<long> sub(std::vector<long> &first, std::vector<long> &second, const
 
 
 
-std::vector<long> naiveMult(std::vector<long> &first, std::vector<long> &second, const long base)
+std::vector<long long> naiveMult(std::vector<long long> &first, std::vector<long long> &second, const long long base)
 // Перемножение столбиком
 {
-    const long len1 = first.size();
-    const long len2 = second.size();
-    std::vector<long> result(len1 + len2 + 1, 0);
+    const long long len1 = first.size();
+    const long long len2 = second.size();
+    std::vector<long long> result(len1 + len2 + 1, 0);
 
-    for (long i = 0; i < len1; i++) {
-        for (long j = 0; j < len2; j++) {
+    for (long long i = 0; i < len1; i++) {
+        for (long long j = 0; j < len2; j++) {
             result[i + j] += first[i] * second[j];
             result[i + j + 1] += result[i + j] / base;
             result[i + j] %= base;
@@ -58,13 +58,13 @@ std::vector<long> naiveMult(std::vector<long> &first, std::vector<long> &second,
     return result;
 }
 
-std::vector<long> karatsubaMult(std::vector<long> &first, std::vector<long> &second, const long base)
+std::vector<long long> karatsubaMult(std::vector<long long> &first, std::vector<long long> &second, const long long base)
 // Метод карацубы
 {
     
-    const long len1 = first.size();
-    const long len2 = first.size();
-    long len = std::max(len1, len2); // Все будет относительно максимального числа
+    const long long len1 = first.size();
+    const long long len2 = first.size();
+    long long len = std::max(len1, len2); // Все будет относительно максимального числа
     // Если длина числа нечетная, то нужно довести до четного состояния
     len = (len % 2 == 0) ? len : len + 1;
 
@@ -72,30 +72,30 @@ std::vector<long> karatsubaMult(std::vector<long> &first, std::vector<long> &sec
     /*Для маленьких чисел данный алгоритм не имеет смысла, т.к. даже наивный алгоритм целесообразнее (быстрее)*/
     if (len <= 10) return naiveMult(first, second, base); // Литерал нужно подобрать, протестировав данные
 
-    long halflen = len / 2;
-    std::vector<long> firstLow(first); // малый разряд, наполняем разрядами из числа first
+    long long halflen = len / 2;
+    std::vector<long long> firstLow(first); // малый разряд, наполняем разрядами из числа first
     firstLow.resize(halflen, 0); // Оставляем только пол массива, заполняем нулями
-    std::vector<long> SecondLow(second); // малый разряд
+    std::vector<long long> SecondLow(second); // малый разряд
     SecondLow.resize(halflen, 0);
-    std::vector<long> FirstHigh(len1 > halflen ? first.begin() + halflen : first.end(), first.end()); // Старший разряд, нужно начать с половинки, для этого надо проверить
+    std::vector<long long> FirstHigh(len1 > halflen ? first.begin() + halflen : first.end(), first.end()); // Старший разряд, нужно начать с половинки, для этого надо проверить
     FirstHigh.resize(halflen, 0);
-    std::vector<long> SecondHigh(len2 > halflen ? second.begin() + halflen : second.end(), second.end()); // Старший разряд, нужно начать с половинки, для этого надо проверить
+    std::vector<long long> SecondHigh(len2 > halflen ? second.begin() + halflen : second.end(), second.end()); // Старший разряд, нужно начать с половинки, для этого надо проверить
     SecondHigh.resize(halflen, 0);
 
-    std::vector<long> multLow = karatsubaMult(firstLow, SecondLow, base);
-    std::vector<long> multHigh = karatsubaMult(FirstHigh, SecondHigh, base);
+    std::vector<long long> multLow = karatsubaMult(firstLow, SecondLow, base);
+    std::vector<long long> multHigh = karatsubaMult(FirstHigh, SecondHigh, base);
 
-    std::vector<long> firstSum = add(firstLow, FirstHigh, base);
-    std::vector<long> SecondSum = add(SecondLow, SecondHigh, base);
+    std::vector<long long> firstSum = add(firstLow, FirstHigh, base);
+    std::vector<long long> SecondSum = add(SecondLow, SecondHigh, base);
 
-    std::vector<long> multMid = karatsubaMult(firstSum, SecondSum, base);
+    std::vector<long long> multMid = karatsubaMult(firstSum, SecondSum, base);
 
     multMid = sub(multMid, multLow, base);
     multMid = sub(multMid, multHigh, base);
 
-    std::vector<long> result(2 * len, 0);
+    std::vector<long long> result(2 * len, 0);
 
-    for (long i = 0; i < 2 * len - 1; i++) {
+    for (long long i = 0; i < 2 * len - 1; i++) {
         result[i] += (i < multLow.size()) ? multLow[i] : 0;
         result[i] += (i >= halflen && i < halflen + multMid.size()) ? multMid[i - halflen] : 0;
         result[i] += (i >= len && i < len + multHigh.size()) ? multHigh[i - len] : 0;
--- main.cpp
+++ main.cpp
@@ -6,21 +6,21 @@
 #include <chrono>
 
 
-std::string getStringNumber(const long len)
+std::string getStringNumber(const long long len)
 {
     std::string result;
-    for (long i = 0; i < len; i++) {
+    for (long long i = 0; i < len; i++) {
         result.push_back(char(48 + rand() % 10));
     }
     return result;
 }
 
-std::vector<long> stringToVector(const std::string str, const long digitlen)
+std::vector<long long> stringToVector(const std::string str, const long long digitlen)
 {
-    long len = str.length();
-    std::vector<long> result(long(std::ceil(len / double(digitlen))), 0);
+    long long len = str.length();
+    std::vector<long long> result(long(std::ceil(len / double(digitlen))), 0);
 
-    for (long i = 0; i < len / digitlen; i++) {
+    for (long long i = 0; i < len / digitlen; i++) {
         result[i] = std::stol(str.substr(len - (i + 1) * digitlen, digitlen));
     }
 
@@ -31,18 +31,18 @@ std::vector<long> stringToVector(const std::string str, const long digitlen)
     return result;
 }
 
-void printVector(const std::vector<long> vec)
+void printVector(const std::vector<long long> vec)
 {
-    for (long i = 0; i < vec.size(); i++) std::cout << vec[i] << ' ';
+    for (long long i = 0; i < vec.size(); i++) std::cout << vec[i] << ' ';
     std::cout << std::endl;
 }
 
-long hashFunction(const std::vector<long> vec)
+long long hashFunction(const std::vector<long long> vec)
 {
-    long result = 0;
-    long q = 123456789;
-    long temp;
-    for (long i = 0; i < vec.size(); i++) {
+    long long result = 0;
+    long long q = 123456789;
+    long long temp;
+    for (long long i = 0; i < vec.size(); i++) {
         temp = vec[i];
         while (temp > 0) {
             result += temp % 10;
@@ -57,24 +57,24 @@ long hashFunction(const std::vector<long> vec)
 
 int main()
 {
-    const long max_len = 1000;
-    const long tests = 1;
-    const long digit_len = 9;
-    const long base = std::pow(10, digit_len);
+    const long long max_len = 1000;
+    const long long tests = 1;
+    const long long digit_len = 9;
+    const long long base = std::pow(10, digit_len);
 
-    for (long i = 10; i < max_len; i *= 2) {
+    for (long long i = 10; i < max_len; i *= 2) {
         srand(0);
         std::string firstString = getStringNumber(i);
         std::string secondString = getStringNumber(i);
         std::cout << firstString << " " << secondString << std::endl; //DEBUG
-        std::vector<long> firstVector = stringToVector(firstString, digit_len);
-        std::vector<long> secondVector = stringToVector(secondString, digit_len);
+        std::vector<long long> firstVector = stringToVector(firstString, digit_len);
+        std::vector<long long> secondVector = stringToVector(secondString, digit_len);
         printVector(firstVector); //DEBUG
         printVector(secondVector); //DEBUG
 
         auto start = std::chrono::high_resolution_clock::now();
-        for (long j = 0; j < tests; j++) {
-            std::vector<long> result =  karatsubaMult(firstVector, secondVector, base);
+        for (long long j = 0; j < tests; j++) {
+            std::vector<long long> result =  naiveMult(firstVector, secondVector, base);
             printVector(result); //DEBUG
             std::cout << "HashSum: " << hashFunction(result) << std::endl; //DEBUG
         }
